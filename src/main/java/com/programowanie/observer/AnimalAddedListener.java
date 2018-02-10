91b380a386dc25de2b746c4bbda93e0e2fb7ab3d
Grzegorz Kubiak <kingregor@gmail.com>
2018-02-10 14:54:15 +0100

    zmiana katalogu

---
+++ src/main/java/com/programowanie/observer/AnimalAddedListener.java
@@ -0,0 +1,8 @@
+package com.programowanie.observer;
+
+public interface AnimalAddedListener {
+
+    public void onAnimalAdded(Animal animal){
+
+    }
+}
---
+++ src/main/java/com/programowanie/observer/Zoo.java
@@ -0,0 +1,11 @@
+package com.programowanie.observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+    private List<Animal> animals = new ArrayList<>();
+    private List<AnimalAddedListener> listenerts = new ArrayList<>();
+
+    
+}
